6345744826e8b4bf4f78cb8edc000bf78a96d5d0
Angel Alegre <aalegre.qa@gmail.com>
2023-06-19 15:28:40 -0500

    strings with backsticks xd d

--- string.js
+++ string.js
@@ -6,6 +6,6 @@ console.log("string con comiilas dobles");
 //backsticks - ´
 const name = "Angel";
 const age = 28;
-const text = "Hola";
+const text = "Hola ";
 
 console.log(`My name is ${name} and my age is ${age}`);
