1b51be841ebd3b1f1d6abf8316d46a97263f76fc
Angel Alegre <aalegre.qa@gmail.com>
2023-06-19 14:44:18 -0500

    strings with backsticks

---
+++ string.js
@@ -0,0 +1,10 @@
+//strings
+console.log("string con comillas simples");
+
+console.log("string con comiilas dobles");
+
+//backsticks - ´
+const name = "Angel";
+const age = 28;
+
+console.log(`My name is ${name} and my age is ${age}`);
